56502f20eeaa1ba6133f61a59d9103c90300e011
jayk293 <jason_cookie_dude@yahoo.co.uk>
2024-11-22 22:02:56 +0000

    wip

---
+++ src/lib/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import Tab, { Label } from '@smui/tab';
+    import TabBar from '@smui/tab-bar';
+    import Calculation from './Calculation.svelte'
+
+    export let active = "Plus";
+
+</script>
+
+<TabBar tabs={['Plus', 'Minus', 'Powers']} let:tab bind:active>
+    <!-- Note: the `tab` property is required! -->
+    <Tab {tab}>
+      <Label>{tab}</Label>
+    </Tab>
+  </TabBar>
+ 
+  {#if active === 'Plus'}
+  <Calculation let:value1 let:value2>
+    <p>{value1} plus {value2} is {value1 + value2}</p>
+  </Calculation>  
+  {:else if active === 'Minus'}
+  <Calculation let:value1 let:value2>
+    <p>{value1} minus {value2} is {value1 - value2}</p>
+  </Calculation>
+  {:else if active === 'Powers'}
+  <Calculation let:value1 let:value2>
+    <p>{value1} to the power of {value2} is {Math.pow(value1, value2)}</p>
+  </Calculation>
+
+  {/if}
+
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,34 @@
 <script>
-  import { base } from "$app/paths";
+	import Counter from '$lib/App.svelte';
 </script>
 
-<h1>sveltekit-gh-pages</h1>
-<p>Deployed to GitHub Pages.</p>
-<a href="{base}/about">About</a>
+<main>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/svelte-material-ui@7.0.0/bare.min.css"
+	/>
+
+	<h1> Ali Grice's Big Head</h1>
+	<div class="example">
+		<Counter />	
+	</div>
+</main>
+
+<style>
+	main {
+		max-width: 600px;
+		margin: 0 auto;
+		padding: 1em;
+	}
+
+	.example {
+		border: 1px solid #ccc;
+		padding: 1em;
+		margin: 1em 0;
+	}
+
+	.purple {
+		color: purple;
+		font-weight: bold;
+	}
+</style>
