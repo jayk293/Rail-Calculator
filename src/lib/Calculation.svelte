f8dd78f710d0e3733a00912998d448398ccf9800
jayk293 <jason_cookie_dude@yahoo.co.uk>
2024-12-06 22:25:42 +0000

    replaced binding results with a button

--- src/lib/Calculation.svelte
+++ src/lib/Calculation.svelte
@@ -8,6 +8,19 @@
 
   /** @type {Props} */
   let { value1 = $bindable(), value2 = $bindable(), children } = $props();
+
+  let result = $state();
+
+  function calculateSum() {
+    const num1 = Number(value1);
+    const num2 = Number(value2);
+
+    if (!isNaN(num1) && !isNaN(num2)) {
+      result = `Sum: ${num1 + num2}`;
+    } else {
+      console.error("Please enter valid numbers");
+    }
+  }
 </script>
 
 <input
@@ -15,7 +28,7 @@
   type="number"
   bind:value={value1}
   placeholder="Enter number..."
-  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+  class="w-full px-4 py-3 rounded-lg border border-gray-300 focus:border-blue-500 focus:ring-0 focus:ring-blue-200 text-base font-medium outline-none transition duration-500 ease-in-out transform bg-white dark:bg-gray-800 text-gray-800 dark:text-white"
 />
 
 <input
@@ -23,14 +36,22 @@
   type="number"
   bind:value={value2}
   placeholder="Enter number..."
-  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+  class="w-full px-4 py-3 rounded-lg border border-gray-300 focus:border-blue-500 focus:ring-0 focus:ring-blue-200 text-base font-medium outline-none transition duration-500 ease-in-out transform bg-white dark:bg-gray-800 text-gray-800 dark:text-white"
 />
 
-{#if children}{@render children({ value1, value2 })}
-{:else}
-  <p>
-    The sum is {value1 + value2}
-  </p>{/if}
+<button
+  onclick={calculateSum}
+  class="mt-4 w-full px-4 py-3 bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 text-white rounded-lg font-semibold text-base transition duration-500 ease-in-out transform"
+>
+  Calculate Sum
+</button>
+
+<div class="bg-white dark:bg-gray-900 p-6 mt-8 rounded-lg shadow-md">
+  <p class="text-xl font-bold mb-2">Result:</p>
+  <p class="text-2xl font-bold text-center text-gray-800 dark:text-white">
+    {result}
+  </p>
+</div>
 
 <style>
   input[type="number"] {
