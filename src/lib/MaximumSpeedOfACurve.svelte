33463941d677cb3bfcf3d11af3232e7aa28e1a96
jayk293 <jason_cookie_dude@yahoo.co.uk>
2024-12-10 20:58:34 +0000

    Create MaximumSpeedOfACurve.svelte

---
+++ src/lib/MaximumSpeedOfACurve.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+  function clear() {
+    value1 = undefined;
+    value2 = undefined;
+    value3 = undefined;
+  }
+
+  function speed(value: number = 0) {
+    return value / 3.6;
+  }
+  function maxCurve(
+    curveRadius: number = 0,
+    cant: number = 0,
+    maxCantDefiency: number = 0,
+  ) {
+    return 0.29 * Math.sqrt(curveRadius * (cant + maxCantDefiency));
+  }
+
+  function roundToNearestDecimal(value) {
+    return Number(value.toFixed(1));
+  }
+  let value1 = $state(undefined);
+  let value2 = $state(undefined);
+  let value3 = $state(undefined);
+  let maximumCurveResult: number | undefined = $derived.by(() => {
+    if (value1 !== undefined && value2 !== undefined && value3 !== undefined) {
+      return roundToNearestDecimal(maxCurve(value1, value2, value3));
+    }
+  });
+
+  let speedResult: number | undefined = $derived.by(() => {
+    if (maximumCurveResult !== undefined) {
+      return roundToNearestDecimal(speed(maximumCurveResult));
+    }
+  });
+</script>
+
+<h2 class="text-3xl font-bold text-center mb-4">Maximum Speed on a Curve</h2>
+
+<input
+  id="number-input1"
+  type="number"
+  bind:value={value1}
+  placeholder="Enter number..."
+  class="w-full px-4 py-3 rounded-lg border border-gray-300 focus:border-blue-500 focus:ring-0 focus:ring-blue-200 text-base font-medium outline-none transition duration-500 ease-in-out transform bg-white dark:bg-gray-800 text-gray-800 dark:text-white"
+/>
+
+<input
+  id="number-input2"
+  type="number"
+  bind:value={value2}
+  placeholder="Enter number..."
+  class="w-full px-4 py-3 rounded-lg border border-gray-300 focus:border-blue-500 focus:ring-0 focus:ring-blue-200 text-base font-medium outline-none transition duration-100 ease-in-out transform bg-white dark:bg-gray-800 text-gray-800 dark:text-white"
+/>
+
+<input
+  id="number-input3"
+  type="number"
+  bind:value={value3}
+  placeholder="Enter number..."
+  class="w-full px-4 py-3 rounded-lg border border-gray-300 focus:border-blue-500 focus:ring-0 focus:ring-blue-200 text-base font-medium outline-none transition duration-100 ease-in-out transform bg-white dark:bg-gray-800 text-gray-800 dark:text-white"
+/>
+<div class="flex justify-center space-x-4">
+  <button
+    disabled={true}
+    class="mt-4 w-full px-4 py-3 bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 text-white rounded-lg font-semibold text-base transition duration-500 ease-in-out transform"
+  >
+    Calculate
+  </button>
+
+  <button
+    onclick={clear}
+    class="mt-4 w-full px-4 py-3 bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 text-white rounded-lg font-semibold text-base transition duration-500 ease-in-out transform"
+  >
+    Clear
+  </button>
+</div>
+
+<div
+  class="bg-white dark:bg-gray-900 p-6 mt-8 rounded-lg shadow-md text-2xl font-bold text-center text-gray-800 dark:text-white"
+>
+  <p>Maximum Curve:</p>
+  <p>{maximumCurveResult}</p>
+</div>
+<div
+  class="bg-white dark:bg-gray-900 p-6 mt-8 rounded-lg shadow-md text-2xl font-bold text-center text-gray-800 dark:text-white"
+>
+  <p>Speed</p>
+  <p>{speedResult}</p>
+</div>
+
+<style>
+  button:disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
+    background-color: #e0e0e0; /* Light gray background */
+    color: #666; /* Darker text color */
+  }
+
+  button:disabled:hover {
+    background-color: #d9d9d9; /* Slightly darker on hover */
+  }
+  input[type="number"] {
+    -webkit-appearance: textfield;
+    appearance: textfield;
+  }
+  input[type="number"]::-webkit-inner-spin-button,
+  input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+</style>
